da54cdeded0fd2c5ea8b15c9a31960d250e5735e
Kneschtie <72698237+Kneschtie@users.noreply.github.com>
2022-07-03 14:09:28 +0200

    repariert

--- nudel_app/test/widget_test.dart
+++ nudel_app/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:nudel_app/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const NudelApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
